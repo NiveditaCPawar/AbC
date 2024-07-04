449ec5dd5f5f53ab616bcedfa1be2da4f73e83cf
NiveditaCPawar <nivipawar1996.gmail.com>
2024-07-04 14:06:08 +0530

    [Nivedita]Added new code

---
+++ src/Pattern/starPattern.java
@@ -0,0 +1,24 @@
+package Pattern;
+
+public class starPattern {
+
+	public static void main(String[] args) {
+//		*
+//		**
+//		***
+//		****
+//		*****
+//		******
+		
+		int star=1;
+		for(int i=1;i<=6;i++) {
+			for(int j=1;j<=star;j++) {
+				System.out.print("*");
+			}star++;
+			System.out.println();
+		}
+		
+
+	}
+
+}
